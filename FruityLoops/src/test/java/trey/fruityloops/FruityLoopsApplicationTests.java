5e522cf100552100ea67f135261b7f1f2fed35a9
Trevon Mays <103228576+TrevonMays@users.noreply.github.com>
2022-10-05 17:03:22 -0700

    Add files via upload

---
+++ FruityLoops/src/test/java/trey/fruityloops/FruityLoopsApplicationTests.java
@@ -0,0 +1,13 @@
+package trey.fruityloops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruityLoopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ FruityLoops/src/test/java/trey/fruityloops/ItemTest.java
@@ -0,0 +1,24 @@
+package trey.fruityloops;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemTest {
+
+    @Test
+    void getPrice() {
+    }
+
+    @Test
+    void setPrice() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void setName() {
+    }
+}
